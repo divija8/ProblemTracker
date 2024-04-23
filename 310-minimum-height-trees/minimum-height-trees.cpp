3c31d4a399213ae52807c524f608c8c5e8313e24
divija8 <162848321+divija8@users.noreply.github.com>
2024-04-23 19:00:36 -0400

    Time: 563 ms (5.02%) | Memory: 69 MB (26.89%) - LeetSync

--- 310-minimum-height-trees/minimum-height-trees.cpp
+++ 310-minimum-height-trees/minimum-height-trees.cpp
@@ -1,39 +1,54 @@
+
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        if(n==1) return {0};
-        if(n==2) return {0, 1};
-        vector<int>ans;
-        vector<vector<int>>adj(n);
-        vector<int>degree(n, 0);
-
-        for(int i=0;i<edges.size();i++) {
-            adj[edges[i][0]].push_back(edges[i][1]);
-            adj[edges[i][1]].push_back(edges[i][0]);
-            degree[edges[i][0]]++;
-            degree[edges[i][1]]++;
+        if(edges.size() == 0) {
+            vector<int> tmp;
+            tmp.push_back(0);
+            return tmp;
         }
-        queue<int> q;
-        for(int i=0;i<n;i++) {
-            if(degree[i]==1) {
-                q.push(i);
+        unordered_map<int, list<int>> adj;
+
+        // creating adjacency list
+        for (int i = 0; i < edges.size(); i++) {
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj[u].push_back(v);
+            adj[v].push_back(u);
+        }
+
+        vector<int> leaves; // Stores current leaf nodes
+
+        // Initialize leaves with nodes having only 1 adjacent node
+        for(auto& d : adj) {
+            if(d.second.size() == 1) {
+                leaves.push_back(d.first);
             }
         }
-        while(!q.empty()) {
-            ans.clear();
-            int k = q.size();
-            while(k--) {
-                int ele = q.front();
-                q.pop();
-                ans.push_back(ele);
-                for(int i=0;i<adj[ele].size();i++) {
-                    degree[adj[ele][i]]--;
-                    if(degree[adj[ele][i]]==1) {
-                        q.push(adj[ele][i]);
-                    };
+
+        // answer can consist of max. 2 nodes (Reason explained above)
+        while(n > 2) {
+            vector<int> new_leaves;
+
+            // remove current leaves
+            n -= leaves.size(); 
+
+            for(int leaf : leaves) {
+                // get the only neighbour of leaf
+                int neighbor = adj[leaf].front(); 
+                // remove leaf from neighbour's adjacency list
+                adj[neighbor].remove(leaf); 
+                
+                // if the adjacent node becomes a leaf node after removal, add it to the queue.
+                if(adj[neighbor].size() == 1) {
+                    new_leaves.push_back(neighbor); 
                 }
             }
+            
+            // update current no of leaf nodes
+            leaves = new_leaves; 
         }
-        return ans;
+
+        return leaves;
     }
 };
