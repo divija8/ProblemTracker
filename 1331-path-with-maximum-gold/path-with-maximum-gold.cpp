eccd77e1e4e5112671c286fa2e1363ba502ef3ff
divija8 <162848321+divija8@users.noreply.github.com>
2024-05-14 18:22:29 -0400

    Time: 131 ms (60.80%) | Memory: 9.3 MB (54.33%) - LeetSync

--- 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
+++ 1331-path-with-maximum-gold/path-with-maximum-gold.cpp
@@ -1,37 +1,33 @@
 class Solution {
 public:
-    vector<int> roww = {1, -1, 0, 0};
-    vector<int> coll = {0, 0, -1, 1};
-    int maxGold = 0;
-
-    int dfs(vector<vector<int>>& grid, int x, int y, int n, int m) {
-        if (x < 0 || x >= n || y < 0 || y >= m || grid[x][y] == 0) return 0;
-        
-        int curr = grid[x][y];
-        grid[x][y] = 0;
-        int localMaxGold = curr;
-
-        for (int i = 0; i < 4; i++) {
-            int newX = x + roww[i];
-            int newY = y + coll[i];
-            localMaxGold = max(localMaxGold, curr + dfs(grid, newX, newY, n, m));
-        }
-
-        grid[x][y] = curr;
-        return localMaxGold;
-    }
-
-    int getMaximumGold(vector<vector<int>>& grid) {
-        int n = grid.size(), m = grid[0].size();
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (grid[i][j] != 0) {
-                    maxGold = max(maxGold, dfs(grid, i, j, n, m));
-                }
+    vector<vector<int>> next = {{0,1},{0,-1},{1,0},{-1,0}};
+    int getMaximumGold(vector<vector<int>>& g) {
+        int res =0,n=g.size(),m=g[0].size();
+        for(int i=0;i<n;i++) {
+            for(int j=0;j<m;j++) {
+                res = max(res, backTrack(g,i,j,n,m));
             }
         }
+        return res;
+    }
 
-        return maxGold;
+    int backTrack(vector<vector<int>>& g, int r, int c, int n, int m) {
+        if(!isValid(r, c, n, m) || g[r][c]==0) return 0;
+        int currVal = g[r][c];
+        g[r][c]=0;
+        int res = currVal;
+        int nextRes = 0;
+        for(int i=0;i<4;i++) {
+            int nextR = r + next[i][0];
+            int nextC = c + next[i][1];
+            nextRes = max(backTrack(g,nextR, nextC,n,m), nextRes);
+        }
+        g[r][c]=currVal;
+        return res + nextRes;
+    }
+
+    bool isValid(int r, int c, int n, int m) {
+        bool res = (r>=0 && c>=0 && r<n && c<m);
+        return res;
     }
 };
