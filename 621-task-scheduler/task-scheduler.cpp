59816e122c831ac6ac96fba55e081b7cc7db2e6c
divija8 <162848321+divija8@users.noreply.github.com>
2024-07-15 07:00:57 -0400

    Time: 120 ms (8.64%) | Memory: 62.5 MB (5.15%) - LeetSync

--- 621-task-scheduler/task-scheduler.cpp
+++ 621-task-scheduler/task-scheduler.cpp
@@ -1,29 +1,55 @@
 class Solution {
 public:
     int leastInterval(vector<char>& tasks, int n) {
-        if (n == 0) return tasks.size();
+//         if (n == 0) return tasks.size();
 
-        unordered_map<char, int> freqMap;
-        int maxFreq = 0;
-        int maxFreqCount = 0;
+//         unordered_map<char, int> freqMap;
+//         int maxFreq = 0;
+//         int maxFreqCount = 0;
 
-        for (char task : tasks) {
+//         for (char task : tasks) {
             
-            if (freqMap.find(task) != freqMap.end()) {
-                freqMap[task]++;
-            } else {
-                freqMap[task] = 1; 
-            }
+//             if (freqMap.find(task) != freqMap.end()) {
+//                 freqMap[task]++;
+//             } else {
+//                 freqMap[task] = 1; 
+//             }
 
-            if (freqMap[task] > maxFreq) {
-                maxFreq = freqMap[task];
-                maxFreqCount = 1;
-            } else if (freqMap[task] == maxFreq) {
-                maxFreqCount++;
+//             if (freqMap[task] > maxFreq) {
+//                 maxFreq = freqMap[task];
+//                 maxFreqCount = 1;
+//             } else if (freqMap[task] == maxFreq) {
+//                 maxFreqCount++;
+//             }
+//         }
+
+//         int totalIntervals = (maxFreq - 1) * (n + 1) + maxFreqCount;
+//         return max(totalIntervals, (int)tasks.size());
+        
+        
+        unordered_map<char, int>cmap;
+        priority_queue<int>pq;
+        queue<vector<int>>q;
+        for(auto i : tasks){
+            cmap[i]++;
+        }
+        for(auto i : cmap){
+            pq.push(i.second);
+        }
+        int time = 0;
+        while(!q.empty() || !pq.empty()){
+            ++time;
+            if(!pq.empty()){
+                if(pq.top()-1){
+                    q.push({pq.top()-1, time+n});
+                }
+                pq.pop();
+            }
+            if (!q.empty() && q.front()[1] == time){
+                pq.push(q.front()[0]);
+                q.pop();
             }
         }
-
-        int totalIntervals = (maxFreq - 1) * (n + 1) + maxFreqCount;
-        return max(totalIntervals, (int)tasks.size());
+        return time;
     }
 };
