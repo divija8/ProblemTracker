dcd6f7671105f7466d878809d37d29d34e2cbc43
divija8 <162848321+divija8@users.noreply.github.com>
2024-07-22 20:20:44 -0400

    Time: 11 ms (17.92%) | Memory: 15.2 MB (8.78%) - LeetSync

--- 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
+++ 1741-sort-array-by-increasing-frequency/sort-array-by-increasing-frequency.cpp
@@ -1,37 +1,39 @@
-#define ppi pair<int,int>
+#define pi pair<int,int> 
+class Compare {
+public:
+    bool operator()(pi below, pi above)
+    {
+        if (below.first > above.first) {
+            return true;
+        }
+        else if (below.first == above.first
+                 && below.second < above.second) {
+            return true;
+        }
+        return false;
+    }
+};
+
 
 class Solution {
-public:
-    class Compare{
-        public:
-        bool operator()(ppi below, ppi above){
-            if(below.first == above.first){ 
-                return below.second < above.second; 
-            }
-            return below.first > above.first;
-        }
-    };
-    vector<int> frequencySort(vector<int>& nums) {
-        vector<int>ans;
+    public:
+    vector<int>frequencySort(vector<int>&nums){
         unordered_map<int,int>cmap;
         for(auto i : nums){
             cmap[i]++;
         }
-        priority_queue<pair<int,int>, vector<pair<int,int>>, Compare>pq;
+        priority_queue<pi,vector<pi>,Compare>pq;
         for(auto i : cmap){
             pq.push({i.second, i.first});
         }
-        while(pq.size() > 0){
-            int freq = pq.top().first;
-            int val = pq.top().second;
-            
-            for(int i=0; i<freq; i++){
-                ans.push_back(val);
-            }
-            
+        int i = 0;
+        while(!pq.empty())
+        {
+            pi top = pq.top();
             pq.pop();
+            while(top.first--)
+                nums[i++] = top.second;
         }
-        
-        return ans;
+        return nums;
     }
 };
