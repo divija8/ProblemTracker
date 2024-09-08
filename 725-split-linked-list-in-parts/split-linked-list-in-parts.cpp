031b04fa1b1d95828c0a515e679c656895d1e0eb
divija8 <162848321+divija8@users.noreply.github.com>
2024-09-08 19:56:54 -0400

    Time: 3 ms (89.15%) | Memory: 14 MB (13.51%) - LeetSync

--- 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
+++ 725-split-linked-list-in-parts/split-linked-list-in-parts.cpp
@@ -25,7 +25,7 @@ public:
         int len = 0;
         for (ListNode* node = root; node; node = node->next)
             len++;
-        int n = len / k, r = len % k; 
+        int n = len / k, r = len % k; // n : minimum guaranteed part size; r : extra nodes spread to the first r parts;
         ListNode* node = root, *prev = nullptr;
         for (int i = 0; node && i < k; i++, r--) {
             parts[i] = node;
