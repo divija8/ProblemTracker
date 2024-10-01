f3935e043d12953cb38c39f6d080a1acb9d16af3
divija8 <162848321+divija8@users.noreply.github.com>
2024-10-01 09:34:10 -0400

    Time: 3 ms (78.32%) | Memory: 14 MB (33.77%) - LeetSync

--- 443-string-compression/string-compression.cpp
+++ 443-string-compression/string-compression.cpp
@@ -3,7 +3,7 @@ public:
     int compress(vector<char>& chars) {
         int ans=0;
         for(int i=0;i<chars.size();) {
-            char letter=chars[i];
+            const char letter=chars[i];
             int count=0;
             while(i<chars.size() && chars[i]==letter) {
                 count++;
@@ -11,7 +11,7 @@ public:
             }
             chars[ans++]=letter;
             if(count>1) {
-                for(char c : to_string(count)) {
+                for(const char c : to_string(count)) {
                     chars[ans++]=c;
                 }
             }
