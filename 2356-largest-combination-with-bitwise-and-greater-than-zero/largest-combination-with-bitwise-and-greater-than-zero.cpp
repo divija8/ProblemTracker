5948ca8ebce1512659c09d99a42bb1cd8102ae6d
divija8 <162848321+divija8@users.noreply.github.com>
2024-11-07 10:50:17 -0500

    Time: 33 ms (47.76%) | Memory: 60 MB (99.40%) - LeetSync

--- 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
+++ 2356-largest-combination-with-bitwise-and-greater-than-zero/largest-combination-with-bitwise-and-greater-than-zero.cpp
@@ -2,16 +2,13 @@ class Solution {
 public:
     int largestCombination(vector<int>& cd) {
         int ans = 1;
-        // unordered_map<int, int>u;
         for(int i=0;i<=31;i++) {
             int cnt = 0;
-            // cout<<i<<": ";
             for(int j=0;j<cd.size();j++) {
                 if(cd[j]>>i&1) {
                     cnt++;
                 }
             }
-            // cout<<endl;
             ans = max(ans, cnt);
         }
         return ans;
