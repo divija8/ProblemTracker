16694a7cd3591bb45919eabe6871bd449b220a93
divija8 <162848321+divija8@users.noreply.github.com>
2024-11-09 10:40:05 -0500

    Time: 0 ms (100.00%) | Memory: 9.1 MB (98.04%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n + 1); 
-        for (int j = 0; j <= n; j++) {
-            int count = 0;
-            int num = j;
-            while (num > 0) {
-                count += num & 1; 
-                num >>= 1;        
-            }
-            ans[j] = count;
-        }
-        return ans;
+        vector<int>ans(n+1, 0);
+       for(int i = 1 ;i<=n; i++){
+            ans[i] = ans[i >> 1] + (i & 1);
+       }
+       return ans;
     }
 };
