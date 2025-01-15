1183bcb731180c9598bc9f7993d4237f08d267d5
divija8 <162848321+divija8@users.noreply.github.com>
2025-01-15 14:19:29 +0530

    Time: 3 ms (69.52%) | Memory: 12.8 MB (90.31%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.cpp
+++ 22-generate-parentheses/generate-parentheses.cpp
@@ -1,21 +1,27 @@
 class Solution {
 public:
-    void generate(int n, int open, int close, string str, vector<string>&result){
-        if(open ==n && close==n){
-            result.push_back(str);
+    void backtrack(int openN, int closedN, int n, vector<string>& res, string& stack) {
+        if (openN == closedN && openN == n) {
+            res.push_back(stack);
             return;
         }
-        if(open<n){
-            generate(n, open+1, close, str + '(', result);
+
+        if (openN < n) {
+            stack += '(';
+            backtrack(openN + 1, closedN, n, res, stack);
+            stack.pop_back();
         }
-        if(close<open){
-            generate(n, open, close+1, str + ')', result);
+        if (closedN < openN) {
+            stack += ')';
+            backtrack(openN, closedN + 1, n, res, stack);
+            stack.pop_back();
         }
-           
     }
+
     vector<string> generateParenthesis(int n) {
-        vector<string>result;
-        generate(n, 0,0,"", result);
-        return result;
+        vector<string> res;
+        string stack;
+        backtrack(0, 0, n, res, stack);
+        return res;
     }
 };
