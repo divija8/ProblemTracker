c267d46ad9f940f944545b8c4b040e59ef32a3f9
divija8 <162848321+divija8@users.noreply.github.com>
2025-01-25 10:21:13 -0500

    Time: 0 ms (100.00%) | Memory: 17.6 MB (71.06%) - LeetSync

--- 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ 747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -8,7 +8,13 @@ public:
             dp[i] = min(dp[i - 1] + cost[i - 1],
                         dp[i - 2] + cost[i - 2]);
         }
+        // dp[0] = cost[0];
+        // dp[1] = min(cost[1], cost[0]);
+        // for (int i = 2; i < n; i++) {
+        //     dp[i] = cost[i] + min(dp[i - 1], dp[i - 2]);
+        // }
         
+        // return min(dp[n-1], dp[n-2]);
         return dp[n];
     }
 };
