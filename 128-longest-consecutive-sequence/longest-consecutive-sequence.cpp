cb19dc3a4fb240e38e63897b0f83dd1d4954a6b3
divija8 <162848321+divija8@users.noreply.github.com>
2025-01-30 20:32:02 -0500

    Time: 94 ms (33.82%) | Memory: 87.9 MB (8.11%) - LeetSync

--- 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.cpp
@@ -4,12 +4,12 @@ public:
         int ans = 0;
         unordered_set<int>cset(nums.begin(), nums.end());
         for(auto i : cset){
-            if(cset.count(i-1)){
+            if(cset.count(i+1)){
                 continue;
             }
             int t = i;
             int len = 1;
-            while(cset.count(++t)){
+            while(cset.count(--t)){
                 len++;
             }
         ans = max(ans, len);
