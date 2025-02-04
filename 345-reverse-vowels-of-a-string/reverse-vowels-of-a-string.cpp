b8965650959c39f9ee89d7c5326fb652c49d623c
divija8 <162848321+divija8@users.noreply.github.com>
2025-02-04 18:07:56 -0500

    Time: 0 ms (100.00%) | Memory: 10.3 MB (63.83%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -1,23 +1,28 @@
 class Solution {
 public:
-    string reverseVowels(string s) {
-        string v;
-        
-        for(auto c: s) {
-            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' ||
-              c=='A' || c=='E' || c=='I' || c=='O' || c=='U'
-              ) {
-                v.push_back(c);
-            }
+    bool isVowel(char c) {
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c=='u' ||
+        c == 'A' || c == 'E' || c == 'I' || c == 'O' || c=='U'){
+            return true;
         }
-        int n=v.size();
-        for(int i=0;i<s.size();i++) {
-            if(s[i]=='a' || s[i]=='e' || s[i]=='i' || s[i]=='o' || s[i]=='u' ||
-              s[i]=='A' || s[i]=='E' || s[i]=='I' || s[i]=='O' || s[i]=='U') {
-                s[i] = v[--n];
+        return false;
+    }
+    string reverseVowels(string str) {
+        int i = 0, j = str.size()-1;
+        while(i < j ){
+            while ( i < j && !isVowel(str[i])){
+                i++;
             }
+            while(i < j  && !isVowel(str[j])){
+                j--;
+            }
+            if(i < j){
+                swap(str[i], str[j]);
+                i++;
+                j--;
+            }
+            
         }
-        
-        return s;
+        return str;
     }
 };
