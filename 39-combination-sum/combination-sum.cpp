3cf8aa78357dbfb2ad4d3e134b2a9368a6ffcc8e
divija8 <162848321+divija8@users.noreply.github.com>
2025-02-10 14:02:49 -0500

    Time: 0 ms (100.00%) | Memory: 14 MB (74.57%) - LeetSync

--- 39-combination-sum/combination-sum.cpp
+++ 39-combination-sum/combination-sum.cpp
@@ -1,24 +1,24 @@
-
 class Solution {
 public:
-    void dfs(vector<int>& candidates, vector<int> &comb, int i, int tot, int target, vector<vector<int>> &ans) {
-        if(tot == target) {
-            ans.push_back(comb);
+      void cSum(int index, int target, vector<int>&candidates, vector<int>&ds, vector<vector<int>>&ans){
+        if(index == candidates.size()){
+            if(target == 0){
+                ans.push_back(ds);
+            }
             return;
         }
-        if(i >= candidates.size() || tot > target) {
-            return;
+        if(candidates[index] <= target){
+            ds.push_back(candidates[index]);
+            cSum(index, target-candidates[index], candidates, ds, ans);
+            ds.pop_back();
         }
-        comb.push_back(candidates[i]);
-        dfs(candidates, comb, i, tot + candidates[i], target, ans);
-        comb.pop_back();
-        dfs(candidates, comb, i + 1, tot, target, ans);
-    }
+        cSum(index+1, target, candidates, ds, ans);
 
+    }
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<vector<int >> ans;
-        vector<int> comb;
-        dfs(candidates, comb, 0, 0, target, ans);
+        vector<vector<int>>ans;
+        vector<int>ds;
+        cSum(0, target, candidates, ds, ans);
         return ans;
     }
 };
