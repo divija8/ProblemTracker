eb53550ebc6b6909de0221e0c08489b23fcd5122
divija8 <162848321+divija8@users.noreply.github.com>
2025-02-11 04:49:48 -0500

    Time: 0 ms (100.00%) | Memory: 10.7 MB (50.52%) - LeetSync

--- 46-permutations/permutations.cpp
+++ 46-permutations/permutations.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    void generatePerm(vector<int>&nums, vector<int>&ds, vector<vector<int>>&ans, int freq[]){
-        if(ds.size() == nums.size()){
-            ans.push_back(ds);
-            return;
-        }
-        for(int i = 0;i<nums.size();i++){
-            if(!freq[i]){
-                freq[i] = true;
-                ds.push_back(nums[i]);
-                generatePerm(nums, ds, ans, freq);
-                ds.pop_back();
-                freq[i] = false;
-            }
+void genPer(vector<int>&ds, vector<vector<int>>&ans, vector<int>&nums, int freq[]){
+    if(ds.size() == nums.size()){
+        ans.push_back(ds);
+        return;
+    }
+    for(int i = 0;i<nums.size();i++){
+        if(!freq[i]){
+            ds.push_back(nums[i]);
+            freq[i] = 1;
+            genPer(ds, ans, nums, freq);
+            freq[i] = 0;
+            ds.pop_back();
+
         }
     }
+}
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>>ans;
         vector<int>ds;
@@ -22,7 +23,7 @@ public:
         for(int i =0;i<nums.size();i++){
             freq[i] = 0;
         }
-        generatePerm(nums, ds, ans, freq);
+        genPer(ds, ans, nums, freq);
         return ans;
     }
 };
