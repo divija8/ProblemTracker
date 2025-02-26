da5214191e83d8aae989b5a955524dbe67a58b9b
divija8 <162848321+divija8@users.noreply.github.com>
2025-02-26 00:16:17 -0500

    Time: 0 ms (100.00%) | Memory: 28 MB (50.84%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.cpp
@@ -12,27 +12,15 @@
 class Solution {
 public:
     int maxPathSum(TreeNode* root) {
-        maxSum = INT_MIN;
-        gainFromSubtree(root);
-        return maxSum;
+        int maxi = INT_MIN;
+        maxPath(root, maxi);
+        return maxi;   
     }
-
-private:
-    int maxSum;
-    int gainFromSubtree(TreeNode* root) {
-        if (root == nullptr) {
-            return 0;
-        }
-        // add the path sum from left subtree. Note that if the path
-        // sum is negative, we can ignore it, or count it as 0.
-        // This is the reason we use `max` here.
-        int gainFromLeft = max(gainFromSubtree(root->left), 0);
-        // add the path sum from right subtree. 0 if negative
-        int gainFromRight = max(gainFromSubtree(root->right), 0);
-        // if left or right path sum are negative, they are counted
-        // as 0, so this statement takes care of all four scenarios
-        maxSum = max(maxSum, gainFromLeft + gainFromRight + root->val);
-        // return the max sum for a path starting at the root of subtree
-        return max(gainFromLeft + root->val, gainFromRight + root->val);
+    int maxPath(TreeNode* node, int &maxi){
+        if(node == NULL)return 0;
+        int left_sum = max(0, maxPath(node->left, maxi));
+        int right_sum = max(0, maxPath(node->right, maxi));
+        maxi = max(maxi, left_sum + right_sum + node->val);
+        return max(left_sum, right_sum) + node->val;
     }
 };
