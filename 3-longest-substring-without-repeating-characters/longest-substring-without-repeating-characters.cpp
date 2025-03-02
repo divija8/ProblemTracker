37c2b80b4646b9360bf31f1cf533974df8a6dae9
divija8 <162848321+divija8@users.noreply.github.com>
2025-03-01 21:14:27 -0500

    Time: 3 ms (83.33%) | Memory: 11.6 MB (76.76%) - LeetSync

--- 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.cpp
@@ -1,18 +1,31 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        int i = 0, j = 0, max_length = 0;
-        unordered_set<int>cset;
-        while(j<s.size()){
-            if(cset.find(s[j]) == cset.end()){
-                cset.insert(s[j]);
-                max_length = max(max_length, j-i+1);
-                j++;
-            } else {
-                cset.erase(s[i]);
-                i++;
+        // unordered_set<char>cset;
+        // int maxi = INT_MIN;
+        // int j = 0, i = 0;
+        // while(i < s.size()){
+        //     if(cset.find(s[i]) != cset.end()){
+        //         cset.insert(s[i]);
+        //         maxi = max(maxi, i - j + 1);
+        //         i++;
+
+        //     }else {
+        //         cset.erase(s[j]);
+        //         j++;
+        //     }
+        // }
+        // return maxi;
+        unordered_map<char, int>cmap;
+        int j = 0;
+        int maxi = 0;
+        for(int i = 0; i<s.size();i++){
+            if(cmap.find(s[i]) != cmap.end()) {
+                j = max(j, cmap[s[i]] + 1); 
             }
+            cmap[s[i]] = i;
+            maxi = max(maxi, i - j + 1);
         }
-        return max_length;
+        return maxi;
     }
 };
