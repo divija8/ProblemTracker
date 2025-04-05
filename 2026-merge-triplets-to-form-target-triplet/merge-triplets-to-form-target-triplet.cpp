6a7752aa4cbcbdc70ea68efbcfbad228856ab76c
divija8 <162848321+divija8@users.noreply.github.com>
2025-04-05 12:40:53 -0400

    Time: 0 ms (100.00%) | Memory: 150.5 MB (70.56%) - LeetSync

--- 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
+++ 2026-merge-triplets-to-form-target-triplet/merge-triplets-to-form-target-triplet.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool mergeTriplets(vector<vector<int>>& triplets, vector<int>& target) {
-        unordered_set<int>good;
-        for(auto t : triplets){
-            if(t[0] > target[0] || t[1] > target[1] || t[2] > target[2]){
-                continue;
-            }
-            for(int i = 0; i<t.size();i++){
-                if(t[i] == target[i]){
-                    good.insert(i);
-                }
+        vector<int> maxTriplet(3, 0);
+        
+        for (const auto& triplet : triplets) {
+            if (triplet[0] <= target[0] && triplet[1] <= target[1] && triplet[2] <= target[2]) {
+                maxTriplet[0] = max(maxTriplet[0], triplet[0]);
+                maxTriplet[1] = max(maxTriplet[1], triplet[1]);
+                maxTriplet[2] = max(maxTriplet[2], triplet[2]);
             }
         }
-        return good.size() == 3;
+        
+        return maxTriplet == target;
     }
 };
