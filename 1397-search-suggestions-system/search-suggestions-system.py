b0b574030f8910e75c445607be71db4d3ecabed9
divija8 <162848321+divija8@users.noreply.github.com>
2025-05-18 11:04:21 -0400

    Time: 3 ms (96.98%) | Memory: 20.5 MB (76.17%) - LeetSync

--- 1397-search-suggestions-system/search-suggestions-system.py
+++ 1397-search-suggestions-system/search-suggestions-system.py
@@ -1,23 +1,19 @@
-from typing import List
-
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         products.sort()
         l, r = 0, len(products) - 1
-        prefix = ""
         res = []
-
         for i in range(len(searchWord)):
-            prefix += searchWord[i]
-
-            while l <= r and (len(products[l]) < len(prefix) or not products[l].startswith(prefix)):
+            prefix = searchWord[i]
+            while l <= r and (len(products[l]) <= i or products[l][i] != prefix):
                 l += 1
 
-            while l <= r and (len(products[r]) < len(prefix) or not products[r].startswith(prefix)):
+            while l <= r and (len(products[r]) <= i or products[r][i] != prefix):
                 r -= 1
 
-            res.append([])
-            for j in range(l, min(l + 3, r + 1)):
-                res[-1].append(products[j])
-
+            current = []
+            for j in range(3):
+                if l + j <= r :
+                    current.append(products[l + j])
+            res.append(current)
         return res
