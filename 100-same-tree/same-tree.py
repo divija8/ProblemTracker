2da836c3867c3f81857f8117ab2d214d81c99deb
divija8 <162848321+divija8@users.noreply.github.com>
2025-06-20 16:54:59 -0700

    Time: 0 ms (100.00%) | Memory: 17.9 MB (38.29%) - LeetSync

--- 100-same-tree/same-tree.py
+++ 100-same-tree/same-tree.py
@@ -4,34 +4,12 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
-# class Solution:
-#     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-#         if not p or not q:
-#             return False
-        
-#         if not p and not q :
-#             return True
-
-#         if p.val != q.val:
-#             return False
-
-#         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
         if not p and not q:
-            return True  # both are None
-
-        if not p or not q:
-            return False  # one is None, other isn't
-
-        if p.val != q.val:
-            return False  # mismatch in values
-
-        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+            return True
+        
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        
+        return False
