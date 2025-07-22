5db54050e9151d9e663dd4911a68c1747abc51f9
divija8 <162848321+divija8@users.noreply.github.com>
2025-07-21 17:14:43 -0700

    Time: 0 ms (100.00%) | Memory: 21.8 MB (26.37%) - LeetSync

--- 303-range-sum-query-immutable/range-sum-query-immutable.py
+++ 303-range-sum-query-immutable/range-sum-query-immutable.py
@@ -7,7 +7,7 @@ class NumArray:
         for num in nums:
             self.prefix_sum.append(self.prefix_sum[-1] + num)
         
-       
+        print(self.prefix_sum)
 
     def sumRange(self, left: int, right: int) -> int:
         return self.prefix_sum[right + 1] - self.prefix_sum[left]
