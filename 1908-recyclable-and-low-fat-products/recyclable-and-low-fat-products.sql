6a537a8158bb0506545284674aecfa4bea56a9cd
divija8 <162848321+divija8@users.noreply.github.com>
2025-07-28 22:59:09 -0700

    Time: 231 ms (61.70%) | Memory: 0B (100.00%) - LeetSync

--- 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
+++ 1908-recyclable-and-low-fat-products/recyclable-and-low-fat-products.sql
@@ -1,2 +1,5 @@
-# Write your MySQL query statement below
-select product_id from Products where low_fats = 'Y' and recyclable = 'Y'
+-- Write your PostgreSQL query statement below
+select product_id
+from products
+where low_fats = 'Y'
+and recyclable = 'Y'
